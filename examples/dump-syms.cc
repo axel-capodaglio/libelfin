ad3460186ff7de063ca20366f3b94ee27aafa0a6
Davide Capodaglio <davide.capodaglio@axelsw.it>
2023-11-24 17:51:15 +0100

    full sample

--- examples/dump-syms.cc
+++ examples/dump-syms.cc
@@ -20,6 +20,28 @@ int main(int argc, char **argv)
         }*/
 
         elf::elf f(elf::create_mmap_loader(argv[1]));
+
+        // dump sections
+        int i = 0;
+        printf("  [Nr] %-16s %-16s %-16s %s ",
+            "Name", "Type", "Address", "Offset");
+        printf("       %-16s %-16s %-15s %5s %4s %5s\n",
+            "Size", "EntSize", "Flags", "Link", "Info", "Align");
+        for (auto& sec : f.sections()) {
+            auto& hdr = sec.get_hdr();
+            printf("  [%2d] %-16s %-16s %016" PRIx64 " %08" PRIx64 " ", i++,
+                sec.get_name().c_str(),
+                to_string(hdr.type).c_str(),
+                hdr.addr, hdr.offset);
+            printf("       %016zx %016" PRIx64 " %-15s %5s %4d %5" PRIu64 "\n",
+                sec.size(), hdr.entsize,
+                to_string(hdr.flags).c_str(), to_string(hdr.link).c_str(),
+                (int)hdr.info, hdr.addralign);
+        }
+
+        elf::symtab symtab;
+
+        // dump symbols
         for (auto &sec : f.sections()) {
                 if (sec.get_hdr().type != elf::sht::symtab && sec.get_hdr().type != elf::sht::dynsym)
                         continue;
@@ -29,7 +51,8 @@ int main(int argc, char **argv)
                        "Num", "Value", "Size", "Type", "Binding", "Index",
                        "Name");
                 int i = 0;
-                for (auto sym : sec.as_symtab()) {
+                symtab = sec.as_symtab();
+                for (auto sym : symtab) {
                         auto &d = sym.get_data();
                         printf("%6d: %016" PRIx64 " %5" PRId64 " %-7s %-7s %5s %s\n",
                                i++, d.value, d.size,
@@ -40,5 +63,22 @@ int main(int argc, char **argv)
                 }
         }
 
+        // dump relocs
+        for (auto& sec : f.sections()) {
+            if (sec.get_hdr().type != elf::sht::rel)
+                continue;
+
+            printf("Relocations REL '%s':\n", sec.get_name().c_str());
+            printf("%6s: %-10s %-10s %-10s %-10s %s\n",
+                "Num", "Offset", "Info", "Type", "Sym", "Name");
+            int i = 0;
+            auto rels = sec.get_rels();
+            for (const auto& rel : rels) {
+                elf::sym sym = symtab.get_sym(rel.sym_idx());
+
+                printf("%6u) %10x %10x %10u %10u %s\n",
+                    i++, rel.offset(), (uint32_t)rel.get_data().info, rel.rel_type(), rel.sym_idx(), sym.get_name().c_str());
+            }
+        }
         return 0;
 }
